f496dc5be027f21aaf51f60cb98d5fe5ec81776e
mc <mc>
2022-12-12 20:35:42 +0100

    2x speed day2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use std::{fs, io::Result};
 
 // Rock = 0 ; Paper = 1 ; Scissors = 2
@@ -8,8 +7,8 @@ pub fn res(input: &str) -> (u64, u64) {
     let mut res = 0;
     let mut res2 = 0;
     for li in input.lines() {
-        let (abc, xyz) = li.split(' ').collect_tuple().unwrap();
-        let [abc, xyz] = [abc.as_bytes()[0] - b'A', xyz.as_bytes()[0] - b'X'];
+        let li2 = li.as_bytes();
+        let [abc, xyz] = [li2[0] - b'A', li2[2] - b'X'];
         let outcome = (4 + xyz - abc) % 3;
         let outcome2 = (2 + xyz + abc) % 3;
         res += (xyz + 1 + (3 * outcome)) as u64;
