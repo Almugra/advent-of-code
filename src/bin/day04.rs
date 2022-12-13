45f3d732f65ac12c41c706da83e1b490ca125c40
mc <mc>
2022-12-13 15:26:32 +0100

    added p2 solution for d4

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -3,20 +3,23 @@ use std::{fs, io};
 fn main() -> io::Result<()> {
     let file = fs::read_to_string("./inputs/i4.txt").unwrap();
     let start = std::time::Instant::now();
-    let val: u32 = file
-        .lines()
-        .map(|pair| {
-            let a: Vec<_> = pair
-                .split_terminator(&['-', ','][..])
-                .map(|v| v.parse::<u32>().unwrap())
-                .collect();
-            if a[0] <= a[2] && a[1] >= a[3] || a[0] >= a[2] && a[1] <= a[3] {
-                return 1;
-            }
-            0
-        })
-        .sum();
-    println!("Part 1 = {:?}", val);
+    let val = file.lines().map(|pair| {
+        pair.split_terminator(&['-', ','][..])
+            .map(|v| v.parse::<u32>().unwrap())
+            .collect()
+    });
+
+    println!(
+        "Part 1 = {}",
+        val.clone()
+            .map(|a: Vec<_>| (a[0] <= a[2] && a[1] >= a[3] || a[0] >= a[2] && a[1] <= a[3]) as u32)
+            .sum::<u32>()
+    );
+    println!(
+        "Part 2 = {}",
+        val.map(|a| (a[0] <= a[3] && a[2] <= a[1]) as u32)
+            .sum::<u32>()
+    );
     println!("time: {:?}", start.elapsed());
     Ok(())
 }
