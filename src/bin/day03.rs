8a3a5df36ed185c1fb33070fd58b4a70199868ae
mc <mc>
2022-12-13 09:55:57 +0100

    added solution 2 and improved solution 1 of day03

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,26 +1,32 @@
-use std::{fs, io};
+use std::fs;
 
-fn main() -> io::Result<()> {
-    let file = fs::read_to_string("./inputs/i3.txt").unwrap();
-    let start = std::time::Instant::now();
-    let mut res: usize = 0;
-    for line in file.lines() {
-        let bys = line.as_bytes();
-        let (a, b) = bys.split_at(bys.len() / 2);
-        let mut val = true;
-        for x in a {
-            if b.contains(x) && val {
-                val = false;
-                if x >= &97 {
-                    res += (x - 96) as usize;
-                } else {
-                    res += (x - 38) as usize;
-                }
-            }
-        }
-    }
-    println!("Part 1 = {}", res);
-    println!("time: {:?}", start.elapsed());
-    Ok(())
+pub fn parse(compartment: &str) -> u64 {
+    compartment
+        .bytes()
+        .map(|item| match item {
+            b'a'..=b'z' => 1 << (item - 96),
+            b'A'..=b'Z' => 1 << (item - 38),
+            _ => panic!("cant parse"),
+        })
+        .fold(0, |acc, item| acc | item)
 }
 
+fn main() {
+    let file = fs::read_to_string("./inputs/i3.txt").unwrap();
+    let start = std::time::Instant::now();
+    let part_one = file.lines().fold(0, |acc, line| {
+        let (a, b) = line.split_at(line.len() / 2);
+        acc + (parse(a) & parse(b)).trailing_zeros()
+    });
+    println!("Part 1 = {}", part_one);
+
+    let part_two: u32 = file
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|a| (parse(a[0]) & parse(a[1]) & parse(a[2])).trailing_zeros())
+        .sum();
+    println!("Part 2 = {}", part_two);
+
+    println!("time: {:?}", start.elapsed());
+}
